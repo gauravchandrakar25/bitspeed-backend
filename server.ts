1f96aec88307a994c451ec6d031927ff1a4a34b5
Gaurav Chandrakar | GC <34914837+gauravchandrakar25@users.noreply.github.com>
2024-04-04 22:00:28 +0530

    project configured

---
+++ server.ts
@@ -0,0 +1,23 @@
+//Importing project dependancies that we installed earlier
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors' 
+import helmet from 'helmet'
+
+//App Varaibles 
+dotenv.config()
+
+//intializing the express app 
+const app = express(); 
+
+//using the dependancies
+app.use(helmet()); 
+app.use(cors()); 
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000
+
+//Listing to the app and running it on PORT 5000
+app.listen(PORT, async () => {
+   console.log(`listning on port ${PORT}`)
+})
